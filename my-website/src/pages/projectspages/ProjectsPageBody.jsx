eeb8de66441e116b0cb6ceb635f26f0caceb44e9
Dan013577947 <93627888+Dan013577947@users.noreply.github.com>
2025-10-31 16:02:57 +0800

    Made the Header fixed position and included some videos in Projects Page

--- my-website/src/pages/projectspages/ProjectsPageBody.jsx
+++ my-website/src/pages/projectspages/ProjectsPageBody.jsx
@@ -9,45 +9,34 @@ function ProjectsPageBody() {
           Here are some of the projects I’ve built while learning programming concepts and improving my problem-solving skills.
         </p>
         <br />
-        <h3>Skills and Tools</h3>
-        <span className="description">Here are some of the technologies and tools I’m comfortable with:</span>
-        <div className="skills">
-          <i className="fa-brands fa-about test fa-html5">
-            <span className="tooltip"> HTML</span>
-          </i>
-          <i className="fa-brands fa-about fa-css3-alt">
-            <span className="tooltip"> CSS</span>
-          </i>
-          <i className="fa-brands fa-about fa-js">
-            <span className="tooltip"> Javascript</span>
-          </i>
-          <i className="fa-brands fa-about fa-react">
-            <span className="tooltip"> ReactJs</span>
-          </i>
-          <i className="fa-brands fa-about fa-python">
-            <span className="tooltip"> Python</span>
-          </i>
-          <i className="fa-solid fa-about fa-database">
-            <span className="tooltip"> SQL</span>
-          </i>
-        </div>
-        <br />
-        <h3>My Goal</h3>
-        <span className="description">
-          I aim to create web interfaces that not only look great but also provide a seamless user experience. I believe that simplicity, clarity, and performance are the foundations of good web design.
 
-          I’m currently improving my React and Node.js skills while working on personal projects to sharpen my front-end development expertise.
-        </span>
-        <div class="video-container">
+        <div className="video-container">
           <iframe
-            width="560"
-            height="315"
-            src="https://www.youtube.com/watch?v=TNLsxjkwc_Y"
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen>
+            src="https://www.youtube.com/embed/n7Qw__cRfIk"
+            width='500px'
+            height='300px'
+            allowFullScreen
+            frameBorder='0'
+          >
           </iframe>
+
+          <span className="description">
+            InventoProfit is a Final group project and I am the main contributor. I made the program using Python and Tkinter Library for GUI, it has features such as QR code auto generate for each item added, QR code scan using laptop/desktop camera, login and creation account system, forgot password, sales history, daily sales, weekly sales, monthly sales, expenses handling, inventory, and so on. I used txt as database, and used own logic to manipulate the database in txt. The project needed patience and data handling skills.
+          </span>
+        </div>
+        <div className="video-container">
+          <iframe
+            src="https://www.youtube.com/embed/n7Qw__cRfIk"
+            width='500px'
+            height='300px'
+            allowFullScreen
+            frameBorder='0'
+          >
+          </iframe>
+
+          <div className="description">
+            InventoProfit is a Final group project and I am the main contributor. I made the program using Python and Tkinter Library for GUI, it has features such as QR code auto generate for each item added, QR code scan using laptop/desktop camera, login and creation account system, forgot password, sales history, daily sales, weekly sales, monthly sales, expenses handling, inventory, and so on. I used txt as database, and used own logic to manipulate the database in txt. The project needed patience and data handling skills.
+          </div>
         </div>
 
       </div>
